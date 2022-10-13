962296b21184e4da4989cc5a97e9e9c1ef605366
UndertaIe <n604712799@gmail.com>
2022-10-13 11:58:33 +0800

    update sql

--- docs/light_news.sql
+++ docs/light_news.sql
@@ -2,18 +2,19 @@ CREATE DATABASE IF NOT EXISTS `light_news`;
 
 USE `light_news`;
 
-CREATE TABLE IF NOT EXISTS `news_model`(
-    `news_id` INTEGER NOT NULL AUTO_INCREMENT,
-    `news_url` VARCHAR(256) NOT NULL,
-    `title` VARCHAR(64) NOT NULL DEFAULT "",
-    `rank` TINYINT(4) NOT NULL DEFAULT 0,
-    `author` VARCHAR(32) NOT NULL DEFAULT "",
-    `abstract` VARCHAR(256) NOT NULL DEFAULT "",
-    `publish_time` DATETIME NOT NULL DEFAULT "1970-01-01 00:00:00",
-    `is_hot` BOOLEAN NOT NULL DEFAULT FALSE,
-    `img_url` VARCHAR(256) NOT NULL DEFAULT "",
-    `list_url` VARCHAR(256) NOT NULL DEFAULT "",
-    `raw_url` VARCHAR(256) NOT NULL DEFAULT "",
+CREATE TABLE `news_model` (
+  `news_id` int(11) NOT NULL AUTO_INCREMENT,
+  `news_url` varchar(256) NOT NULL,
+  `title` varchar(64) NOT NULL DEFAULT '',
+  `rank` tinyint(4) NOT NULL DEFAULT '0',
+  `author` varchar(32) NOT NULL DEFAULT '',
+  `abstract` varchar(512) NOT NULL DEFAULT '' COMMENT '摘要',
+  `publish_time` datetime NOT NULL DEFAULT '1970-01-01 00:00:00',
+  `is_hot` tinyint(1) NOT NULL DEFAULT '0',
+  `img_url` varchar(256) NOT NULL DEFAULT '',
+  `list_url` varchar(256) NOT NULL DEFAULT '',
+  `raw_url` varchar(256) NOT NULL DEFAULT '',
+  `data_source` varchar(32) NOT NULL DEFAULT '',
   PRIMARY KEY (`news_id`),
   UNIQUE KEY `uk_news_url` (`news_url`)
-)ENGINE=INNODB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7999 DEFAULT CHARSET=utf8;
